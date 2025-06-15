ac02a13e5ca6a3eda9af44f458c460a3e472fa26
Eric050505 <1846688234@qq.com>
2025-06-15 20:13:36 +0800

    init CNN-RNN image caption framework

---
+++ CNN_RNN.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+from torchvision import models
+
+
+class CNNEncoder(nn.Module):
+    def __init__(self, embed_size):
+        super().__init__()
+        resnet = models.resnet18(pretrained=True)
+        modules = list(resnet.children())[:-1]
+        self.resnet = nn.Sequential(*modules)
+        self.linear = nn.Linear(resnet.fc.in_features, embed_size)
+        self.bn = nn.BatchNorm1d(embed_size, momentum=0.01)
+
+    def forward(self, images):
+        with torch.no_grad():
+            features = self.resnet(images)
+            features = features.view(features.size(0), -1)  # 保证是[batch, features]
+        features = self.linear(features)
+        features = self.bn(features)
+        return features
+
+
+class RNNDecoder(nn.Module):
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
+        super().__init__()
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)
+
+    def forward(self, features, captions):
+        embeddings = self.embed(captions)
+        embeddings = torch.cat((features.unsqueeze(1), embeddings), 1)
+        hiddens, _ = self.lstm(embeddings)
+        outputs = self.linear(hiddens)
+        return outputs
+
---
+++ Tokenizer.py
@@ -0,0 +1,29 @@
+from transformers import AutoTokenizer
+import torch
+
+
+class Tokenizer:
+    def __init__(self, pretrained_model_name='Qwen/Qwen3-0.6B', max_len=20):
+        print(f"Loading tokenizer...")
+        self.tokenizer = AutoTokenizer.from_pretrained('./pretrained_models/Qwen3-0.6B')
+
+        self.max_len = max_len
+        self.pad_token_id = self.tokenizer.pad_token_id
+        self.bos_token_id = getattr(self.tokenizer, 'bos_token_id', None)
+        self.eos_token_id = getattr(self.tokenizer, 'eos_token_id', None)
+
+    def encode(self, text, max_len=None):
+        max_len = max_len or self.max_len
+        tokens = self.tokenizer.encode(
+            text,
+            add_special_tokens=True,
+            max_length=max_len,
+            truncation=True,
+            padding='max_length',
+            return_tensors='pt'
+        )
+        return tokens.squeeze(0)
+
+    def decode(self, tokens):
+        tokens = tokens.tolist() if isinstance(tokens, torch.Tensor) else tokens
+        return self.tokenizer.decode(tokens, skip_special_tokens=True)
